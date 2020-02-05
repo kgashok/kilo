f00f5b569b3bcafb0eaf9b6cd7c9b43d9c65c3af
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-02-05 11:48:30 +0000

    Step 8 - echo character by character

--- kilo.c
+++ kilo.c
@@ -1,5 +1,6 @@
 //https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h> 
@@ -33,6 +34,13 @@ int main() {
   enableRawMode(); 
 
   char c; 
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); 
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    // is a control character?
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  } 
   return 0;
 }
