c62a8e61818df8a7684b6d1f96c2dc429fe8e262
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-02-05 11:53:45 +0000

    Step 9 - disabling Ctrl-C and Ctrl-Z

--- kilo.c
+++ kilo.c
@@ -24,7 +24,9 @@ void enableRawMode() {
   //ECHO is a bitflag, defined as 00000000000000000000000000001000 in binary. We use the bitwise-NOT operator (~) on this value to get 11111111111111111111111111110111. We then bitwise-AND this value with the flags field, which forces the fourth bit in the flags field to become 0, and causes every other bit to retain its current value. Flipping bits like this is common in C.
   // Step 7
   //There is an ICANON flag that allows us to turn off canonical mode. This means we will finally be reading input byte-by-byte, instead of line-by-line.
-  raw.c_lflag &= ~(ECHO | ICANON);
+  // Step 9
+  //Disable Ctrl-C and Ctrl-Z using ISIG
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
 
