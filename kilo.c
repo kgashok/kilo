4028f24c099f96b638d420f204d5e306599e88b0
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-02-05 11:33:03 +0000

    Step 5 and NOT operator

--- kilo.c
+++ kilo.c
@@ -1,7 +1,23 @@
 //https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
 
+#include <termios.h> 
 #include <unistd.h>
+
+void enableRawMode() { 
+  struct termios raw;
+  
+  tcgetattr(STDIN_FILENO, &raw); 
+
+  //ECHO is a bitflag, defined as 00000000000000000000000000001000 in binary. We use the bitwise-NOT operator (~) on this value to get 11111111111111111111111111110111. We then bitwise-AND this value with the flags field, which forces the fourth bit in the flags field to become 0, and causes every other bit to retain its current value. Flipping bits like this is common in C.
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
+
+}
+
 int main() { 
+  enableRawMode(); 
+
   char c; 
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); 
   return 0;
