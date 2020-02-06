388c85c3e2a75b75e5578e39cc107833b4da594c
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-02-06 18:50:17 +0000

    Step 33 - now it captures rows

--- kilo.c
+++ kilo.c
@@ -136,15 +136,15 @@ int getCursorPosition(int *rows, int *cols) {
     i++;
   }
   buf[i] = '\0';
-  printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
 
-  editorReadKey();
-  return -1;
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+  return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
-  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+  if (  ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
     // As you might have gathered from the code, there is no simple “move the cursor to the bottom-right corner” command.
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
     return getCursorPosition(rows, cols);
