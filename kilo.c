97806051324d9b9043954e6bb804455373860765
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-02-06 10:08:55 +0000

    Step 24 - clearing screen

--- kilo.c
+++ kilo.c
@@ -33,6 +33,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
@@ -133,6 +135,8 @@ void editorProcessKeypress() {
 
   switch(c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
