7866810437cb921cdf5eab958979c48d0ad17f47
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-02-06 19:56:47 +0000

    Step 44 - cursor movement

--- kilo.c
+++ kilo.c
@@ -34,6 +34,7 @@
 /*** data ***/
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -248,7 +249,11 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  // Step 44
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25l", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
@@ -276,6 +281,9 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
+  // Step 43
+  E.cx = 0; 
+  E.cy = 0; 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
