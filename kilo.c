b29e363e21ad35d79d848d46f6c83b009559c1cf
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-02-06 19:13:20 +0000

    Step 37 -  abuf ready for use

--- kilo.c
+++ kilo.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <stdlib.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -172,6 +173,17 @@ struct abuf {
 
 #define ABUF_INIT {NULL, 0}
 
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
 
 /*** output ***/
 
