7c9003c53c7660f6e56f387a4b01823d2c446662
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-02-13 16:34:07 +0000

    Step 75 fix cursor positioning for horizontal

--- kilo.c
+++ kilo.c
@@ -394,7 +394,8 @@ void editorRefreshScreen() {
   char buf[32];
   // Step 70 - to fix cursor scrolling on the screen
   //snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, 
+                                            (E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
