536da8a6eb568d6fc5961a66d1f54e0cc39c2df5
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-02-13 11:09:02 +0000

    Step 59 - read a file

--- kilo.c
+++ kilo.c
@@ -3,7 +3,13 @@
 // https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
 // https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html
 // https://viewsourcecode.org/snaptoken/kilo/04.aTextViewer.html
+
 /*** includes ***/
+
+// Step 59 - feature test macro
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
 
 #include <ctype.h>
 #include <errno.h>
@@ -233,15 +239,27 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** file i/o ***/ 
 
-void editorOpen() { 
-    char *line = "Hello, world!"; 
-    ssize_t linelen = 13; 
+void editorOpen(char *filename) { 
+    FILE *fp = fopen(filename, "r"); 
+    if (!fp) die("fopen");
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen+1); 
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0'; 
-    E.numrows = 1; 
+    char *line = NULL;
+    size_t linecap = 0; 
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp); 
+    if (linelen != -1){
+        while (linelen >0 && (line[linelen-1] == '\n' || 
+                              line[linelen-1] == '\r'))
+            linelen--;
+    
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen+1); 
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0'; 
+        E.numrows = 1; 
+    }
+    free(line);
+    fclose(fp);
 }
 
 /*** append buffer ***/
@@ -418,14 +436,16 @@ void initEditor() {
   E.cy = 0; 
   E.numrows = 0;
 
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc >= 2)    
+    editorOpen(argv[1]);
 
   while (1) {
     editorRefreshScreen();
