dd581f96c4c18870fccfc3af685cc5a00afb14e5
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-02-13 11:33:44 +0000

    Step 57 to Step 64

--- kilo.c
+++ kilo.c
@@ -67,7 +67,7 @@ struct editorConfig {
   int screenrows;
   int screencols;
   int numrows;
-  erow row;
+  erow *row;
   struct termios orig_termios;
 };
 
@@ -237,6 +237,20 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** row operations ***/ 
+
+void editorAppendRow(char *s, size_t len) { 
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1); 
+  memcpy(E.row[at].chars, s, len); 
+  E.row[at].chars[len] = '\0'; 
+  E.numrows = 1; 
+
+}
+
 /*** file i/o ***/ 
 
 void editorOpen(char *filename) { 
@@ -251,12 +265,7 @@ void editorOpen(char *filename) {
         while (linelen >0 && (line[linelen-1] == '\n' || 
                               line[linelen-1] == '\r'))
             linelen--;
-    
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen+1); 
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0'; 
-        E.numrows = 1; 
+        editorAppendRow(line, linelen);
     }
     free(line);
     fclose(fp);
@@ -293,17 +302,18 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     if (y >= E.numrows) { 
-        if (y == E.screenrows / 3) {
-        // Step 41 
-        char welcome[80]; 
-        int welcomelen = snprintf(welcome, sizeof(welcome), 
-            "Kilo editor -- version %s", KILO_VERSION);
-        if (welcomelen > E.screencols) welcomelen = E.screencols;
-        // Step 42 - centering 
-        int padding = (E.screencols - welcomelen) / 2;
-        if (padding) { 
-            abAppend(ab, "~", 1); 
-            padding--;
+        // Step 60
+        if (E.numrows == 0 && y == E.screenrows / 3) {
+            // Step 41 
+            char welcome[80]; 
+            int welcomelen = snprintf(welcome, sizeof(welcome), 
+                "Kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            // Step 42 - centering 
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) { 
+                abAppend(ab, "~", 1); 
+                padding--;
         }
         while (padding--) abAppend(ab, " ", 1);
         abAppend(ab, welcome, welcomelen);
@@ -311,9 +321,9 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
         }
     } else {
-        int len = E.row.size;
+        int len = E.row[y].size;
         if (len > E.screencols) len = E.screencols; 
-        abAppend(ab, E.row.chars, len);
+        abAppend(ab, E.row[y].chars, len);
     }
 
     // Step 40 one at a time 
@@ -435,6 +445,7 @@ void initEditor() {
   E.cx = 0; 
   E.cy = 0; 
   E.numrows = 0;
+  E.row = NULL;
 
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
