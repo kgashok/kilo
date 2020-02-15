cc632676ab76eb6a2bba7a3bf5b0285d27d00ce6
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-02-15 04:27:28 +0000

    link to the next steps

--- kilo.c
+++ kilo.c
@@ -2,6 +2,7 @@
 
 // https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
 // https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html
+// https://viewsourcecode.org/snaptoken/kilo/04.aTextViewer.html
 
 /*** includes ***/
 
