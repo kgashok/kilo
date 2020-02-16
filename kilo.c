fec7b16d3a7a3b55101bc2e842ff017a819c9126
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-02-16 18:48:25 +0000

    Step 91 - all done except status bar

--- kilo.c
+++ kilo.c
@@ -524,7 +524,9 @@ void editorProcessKeypress() {
     break;
 
   case END_KEY:
-    E.cx = E.screencols - 1;
+    // Step 91
+    if (E.cy < E.numrows)
+      E.cx = E.row[E.cy].size;
     break;
 
   case PAGE_UP:
@@ -536,7 +538,7 @@ void editorProcessKeypress() {
         E.cy = E.rowoff + E.screenrows - 1;
         if (E.cy > E.numrows) E.cy = E.numrows;
       }
-      
+
       int times = E.screenrows;
       while (times--)
         editorMoveCursor(c == PAGE_UP? ARROW_UP : ARROW_DOWN);
