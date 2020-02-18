1f4c3652e0bb074b69ef21f3bc4238c97f150ed6
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-02-18 06:50:28 +0000

    Step 105 and 106 - save to disk

--- kilo.c
+++ kilo.c
@@ -15,6 +15,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -327,6 +328,30 @@ void editorInsertChar(int c) {
 
 /*** file i/o ***/ 
 
+// Step 105 
+// a function that converts our array of erow
+// structs into a single string that is ready
+// to be written out to a file.
+char *editorRowsToString(int *buflen) { 
+    int totlen = 0; 
+    int j; 
+    for (j = 0; j < E.numrows; j++) 
+      totlen += E.row[j].size + 1; 
+    *buflen = totlen;
+
+    char *buf = malloc(totlen);
+    char *p = buf; 
+    for (j  = 0; j < E.numrows; j++) { 
+        memcpy(p, E.row[j].chars, E.row[j].size); 
+        p += E.row[j].size; 
+        *p = '\n'; 
+        p++; 
+    }
+
+    return buf;
+
+}
+
 void editorOpen(char *filename) { 
     free(E.filename);
     E.filename = strdup(filename);
@@ -346,6 +371,19 @@ void editorOpen(char *filename) {
     }
     free(line);
     fclose(fp);
+}
+
+void editorSave() { 
+    if (E.filename == NULL) return;
+
+    int len; 
+    char *buf = editorRowsToString(&len);
+
+    int fd = open(E.filename, O_RDWR | O_CREAT, 0644); 
+    ftruncate(fd, len);
+    write(fd, buf, len);
+    close(fd); 
+    free(buf);
 }
 
 /*** append buffer ***/
